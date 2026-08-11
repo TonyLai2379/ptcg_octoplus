80aa70ed08e65c814bbd47547c6daf82abba17e0
TonyLai2379 <loganlai0422@gmail.com>
2026-08-11 20:10:00 +0800

    Update app.js

--- app.js
+++ app.js
@@ -37,9 +37,9 @@ let feedbackBase64 = "";
 // 🔥 環境熱門牌組一鍵匯入設定
 // ==========================================
 const POPULAR_DECKS = [
-    { name: "化隱", code: "uCRvSM-NdxUvZ-iWAqYI" },
-    { name: "更新中", code: "8xxG8a-WlVb4G-cYYcc8" },
-    { name: "更新中", code: "E8cx88-X1K2K8-YYYccG" },
+    { name: "化隱", code: "DfPGpo-EbFJRT-vzELZu" },
+    { name: "魔靈多龍", code: "rAwHnZ-kLhnNT-EnmbLY" },
+    { name: "袋獸列空坐", code: "TOlRYY-vAKTOT-OFdGqI" },
     { name: "更新中", code: "8Yxx8a-4VwKq8-xcxxYG" },
     { name: "更新中", code: "GcxYc8-aA0F0D-cK8a4G" }
     // 💡 你以後只要在這裡修改名稱跟代碼即可
